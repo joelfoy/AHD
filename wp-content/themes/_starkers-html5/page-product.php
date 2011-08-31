551b649a66297f5b84cdb48f84ba3b5506731d8c
joel <joelfoy@gmail.com>
2011-08-31 10:42:13 -0400

    updates_08312011
    Daily updates

---
+++ wp-content/themes/_starkers-html5/page-product.php
@@ -0,0 +1,28 @@
+<?php
+/*
+Template Name: Products
+*/
+?>
+
+<?php get_header(); ?>
+<article <?php post_class() ?> id="post-<?php the_ID(); ?>">
+<h2>This is the Products Template</h2>
+<?php if(is_page('products'))	{ ?>
+	<?php 
+	// Get Products by Supplement Type
+	$args = array(
+	  'taxonomy'     => 'supplement-type',
+	  'orderby'      => 'name',
+	  'show_count'   => 0,
+	  'pad_counts'   => 0,
+	  'hierarchical' => 1,
+	  'title_li'     => ''
+	); ?>
+	<ul>
+		<?php wp_list_categories($args); ?>
+	</ul>
+</article>	
+<?php } // End Supplement List ?>
+
+		
+<?php get_footer(); ?>
